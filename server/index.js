9eceb40016490ac04b3c067fc57db664b68865b5
Leidy Johanna Martínez Giraldo <martinezgiraldoleidy@gmail.com>
2020-06-17 11:20:20 -0500

    Add cors

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = require("./routes/index");
+const cors = require("cors");
 const { PORT, ENVIRONMENT } = require("./config");
 
 require("./repository/database");
@@ -7,6 +8,9 @@ require("./repository/database");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+// Routes
 app.use("/api", router);
 
 app.listen(PORT, () => {
