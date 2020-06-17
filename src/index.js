71eebb99cff3f9ef5aea5b0b5b5bd5b64ea73eca
Leidy Johanna Martínez Giraldo <martinezgiraldoleidy@gmail.com>
2020-06-16 23:45:38 -0500

    Refactor to initial architecture

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+require("./database");
+
+const app = express();
+app.use(express.json());
+
+const ordersRouter = require("./routes/orders");
+const productsRouter = require("./routes/products");
+const usersRouter = require("./routes/users");
+
+app.use("/api/orders", ordersRouter);
+app.use("/api/orders", productsRouter);
+app.use("/api/users", usersRouter);
+
+app.listen("8080", () => {
+  console.log("Server started");
+});
