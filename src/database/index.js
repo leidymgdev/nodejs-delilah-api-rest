abed4d70c1ff412f33f77154c15b8c9a4b14a978
Leidy Johanna Martínez Giraldo <martinezgiraldoleidy@gmail.com>
2020-06-18 18:56:49 -0500

    Refactor

--- src/database/index.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require("sequelize");
-
-const database = new Sequelize("delilahrestoelena", "root", "Intel135", {
-  host: "localhost",
-  dialect: "mysql",
-});
-
-module.exports = database;
--- src/routes/users.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-
-router.get("/", async (req, res) => {
-  res.send("Home users");
-});
-
-module.exports = router;
