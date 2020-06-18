8e0b131548ce371a4a91d6bf2881db3ef3d73606
Leidy Johanna Martínez Giraldo <martinezgiraldoleidy@gmail.com>
2020-06-18 18:51:53 -0500

    Refactor

--- server/database/index.js
+++ server/database/index.js
@@ -1,8 +1,10 @@
 const Sequelize = require("sequelize");
 
-const database = new Sequelize("delilahrestoelena", "root", "Intel135", {
+const database = new Sequelize("mysql://root:@localhost:3306/delilahresto");
+
+/*const database = new Sequelize("delilahrestoelena", "root", "Intel135", {
   host: "localhost",
   dialect: "mysql",
-});
+});*/
 
 module.exports = database;
